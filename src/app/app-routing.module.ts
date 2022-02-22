5cc2b79d7f1c2412010cd8ec9f8886796f7b3376
mendelmariano <mendelbsi@gmail.com>
2022-02-22 00:11:14 -0300

    Exclusão de produtos e correções de layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,20 +14,12 @@ export class AppRoutingModule { }
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProductCreateComponent } from './components/product/product-create/product-create.component';
-import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
-import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { ResgateFormComponent } from './components/resgate/resgate-form/resgate-form.component';
 import { HomeComponent } from './views/home/home.component';
 import { InvestmentCrudComponent } from './views/investment-crud/investment-crud.component';
-import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
 const routes: Routes = [
   {path:"", component: HomeComponent},
-  {path:"products", component: ProductCrudComponent},
-  {path: "products/create", component: ProductCreateComponent},
-  {path: "products/update/:id", component: ProductUpdateComponent},
-  {path: "products/delete/:id", component: ProductDeleteComponent},
   {path: "investments", component: InvestmentCrudComponent},
   {path: "resgate/update/:nome", component: ResgateFormComponent},
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,26 +12,20 @@ import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import { HomeComponent } from './views/home/home.component';
-import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RedDirective } from './directives/red.directive';
 import { ForDirective } from './directives/for.directive';
-import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatFormFieldModule,} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
-import { ProductReadComponent } from './components/product/product-read/product-read.component';
-import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
-import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
-import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 import { InvestmentCrudComponent } from './views/investment-crud/investment-crud.component';
 import { InvestmentReadComponent } from './components/investment/investment-read/investment-read.component';
 import { ResgateFormComponent } from './components/resgate/resgate-form/resgate-form.component';
@@ -67,14 +61,8 @@ export const customCurrencyMaskConfig = {
     FooterComponent,
     NavComponent,
     HomeComponent,
-    ProductCrudComponent,
     RedDirective,
     ForDirective,
-    ProductCreateComponent,
-    ProductReadComponent,
-    ProductRead2Component,
-    ProductUpdateComponent,
-    ProductDeleteComponent,
     InvestmentCrudComponent,
     InvestmentReadComponent,
     ResgateFormComponent,
